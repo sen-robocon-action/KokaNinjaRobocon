cbb8028c11e6fc8e22eb340f336a1039f7e2b7b1
sen-robocon-action <sen-robocon-action@outlook.jp>
2025-04-19 18:44:34 +0900

    更新 pxt.json, main.blocks, kokaninjarobocon.ts

---
+++ kokaninjarobocon.ts
@@ -0,0 +1,26 @@
+enum MyEnum {
+    //% block="前"
+    前,
+    //% block="後ろ"
+    後ろ,
+    //% block="右"
+    右,
+    //% block="左"
+    左,
+    //% block="止まる"
+    止まる
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#ff7f50 icon=""
+namespace こうかにんじゃロボコン {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+
+}
