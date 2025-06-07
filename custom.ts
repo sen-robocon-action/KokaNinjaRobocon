5b6848b650cba65528373e66b699f8065d26f2cd
sen-robocon-action <sen-robocon-action@outlook.jp>
2025-06-07 17:11:08 +0900

    更新 pxt.json, main.blocks, custom.ts

---
+++ custom.ts
@@ -0,0 +1,42 @@
+
+let speed = 0
+
+enum way {
+    //% block="前"
+    前 = 1,
+    //% block="後ろ"
+    後ろ = -1,
+    //% block="右"
+    右 = 2,
+    //%　block="左"
+    左 = -2,
+    //% block="停止"
+    停止 = 0
+}
+
+namespace こうかにんじゃロボコン{
+    //% block
+    export function 進む方向(speed: number,w: way): void {
+        if(w === 1) {
+            servos.P1.run(-speed)
+            servos.P2.run(speed)   
+        }
+        if(w === -1) {
+            servos.P1.run(speed)
+            servos.P2.run(-speed)   
+        }
+        if(w === 2) {
+            servos.P1.stop()
+            servos.P2.run(speed)   
+        }
+        if(w === -2) {
+            servos.P1.run(-speed)
+            servos.P2.stop()   
+        }
+    }
+
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    }
+}
