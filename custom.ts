29ece86d64ac83d7ec61d606167f6e95520b9b34
sen-robocon-action <sen-robocon-action@outlook.jp>
2025-06-08 16:48:05 +0900

    更新 main.blocks, custom.ts

--- custom.ts
+++ custom.ts
@@ -1,4 +1,4 @@
-
+let ID = 0
 let speed = 0
 
 enum way {
@@ -37,5 +37,48 @@ namespace こうかにんじゃロボコン{
             servos.P1.stop()
             servos.P2.stop()
         }
+    }
+    //% block
+    export function 無線送信(w: way,ID: number): void {
+        if(w === 1) {
+            radio.setGroup(ID)
+            radio.sendString("前")
+        }
+        if(w === -1) {
+            radio.setGroup(ID)
+            radio.sendString("後ろ")   
+        }
+        if(w === 2) {
+            radio.setGroup(ID)
+            radio.sendString("右")   
+        }
+        if(w === -2) {
+            radio.setGroup(ID)
+            radio.sendString("左")   
+        }
+        if(w === 0) {
+            radio.setGroup(ID)
+            radio.sendString("停止")
+        }
+    }
+    //% block
+    export function 無線受信(w: way,ID: number): void {
+        radio.onReceivedString(function(receivedString: string) {   
+        if(w === 1 && receivedString  === "前") {
+            radio.setGroup(ID)
+            }
+        if(w === -1 && receivedString  === "後ろ") {
+            radio.setGroup(ID)  
+            }
+        if(w === 2 && receivedString  === "右") {
+            radio.setGroup(ID)   
+            }
+        if(w === -2 && receivedString  === "左") {
+            radio.setGroup(ID)
+            }
+        if(w === 0 && receivedString  === "停止") {
+            radio.setGroup(ID)
+            }
+        })
     }
 }
